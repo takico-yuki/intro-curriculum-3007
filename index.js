4dbc2e8b22e2933ac38d68dbc0e0917ea14bba24
takico <takita.y1026@gmail.com>
2020-05-31 16:07:11 +0900

    実装完了

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
 
-module.exports = {
-}
+/**
+ * 配列に含まれる整数が17で割り切れるもののみだけにする
+ * 
+ * @param {number} num
+ * @returns {number} 
+ * @author takico
+ */
+function isMultipleOfSeventeen(num) {
+    return num % 17 === 0;
+  }  
+module.exports = {isMultipleOfSeventeen}
